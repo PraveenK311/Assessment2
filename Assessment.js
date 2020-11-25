03b0d70cec2df0cf7f77b27f44dadd29989b0986
kumar311 <67825824+kumar311@users.noreply.github.com>
2020-11-25 13:00:28 +0530

    Add files via upload

---
+++ Assessment.js
@@ -0,0 +1,33 @@
+    //  // Check browser support
+    //  if (typeof(Storage) !== "undefined") {
+    //      // Store
+    //      localStorage.setItem("lastname", "Smith");
+    //      // Retrieve
+    //      document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    //  } else {
+    //      document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+    // //  }
+
+    // window.localStorage
+    // localStorage.setItem("name", "praveen");
+    // var lastname = localStorage.getItem("name");
+    // document.getElementById("result").innerHTML = localStorage.getItem("name");function myFunction() {
+
+    // // dropdown content:
+    // document.getElementById("myDropdown").classList.toggle("show");
+    // }
+
+    // // Close the dropdown if the user clicks outside of it
+    // window.onclick = function(event) {
+    //     if (!event.target.matches('.dropbtn')) {
+    //         var dropdowns = document.getElementsByClassName("dropdown-content");
+    //         var i;
+    //         for (i = 0; i < dropdowns.length; i++) {
+    //             var openDropdown = dropdowns[i];
+    //             if (openDropdown.classList.contains('show')) {
+    //                 openDropdown.classList.remove('show');
+    //             }
+    //         }
+    //     }
+    // }
+    //
